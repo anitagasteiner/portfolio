dbf3e43ad5c2eb0b0023e1debc19f4c34247da01
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-01-15 12:33:30 +0100

    skills images animation finished

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -1,12 +1,69 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SkillComponent } from './skill/skill.component';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    SkillComponent    
+  ],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+
+  skills = [
+    {
+      image: "./../../../assets/img/angular.svg",
+      name: "Angular"
+    },
+    {
+      image: "./../../../assets/img/type-script.svg",
+      name: "TypeScript"
+    },
+    {
+      image: "./../../../assets/img/java-script.svg",
+      name: "JavaScript"
+    },
+
+    {
+      image: "./../../../assets/img/html.svg",
+      name: "HTML"
+    },
+    {
+      image: "./../../../assets/img/scss.svg",
+      name: "SCSS"
+    },
+    {
+      image: "./../../../assets/img/css.svg",
+      name: "CSS"
+    },
+    {
+      image: "./../../../assets/img/firebase.svg",
+      name: "Firebase"
+    },
+    {
+      image: "./../../../assets/img/git.svg",
+      name: "Git"
+    },
+    {
+      image: "./../../../assets/img/rest-api.svg",
+      name: "REST-API"
+    },
+    {
+      image: "./../../../assets/img/scrum.svg",
+      name: "Scrum"
+    },
+    {
+      image: "./../../../assets/img/material-design.svg",
+      name: "Material Design"
+    },
+    {
+      image: "./../../../assets/img/php.svg",
+      name: "PHP"
+    }
+  ];
 
 }
