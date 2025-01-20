c8f04436140f8adbd9dfe2ccd37f85bc3ea9ccc4
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-01-20 21:57:27 +0100

    portfolio animation completed

--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -9,10 +9,11 @@ import { Component, Input, AfterViewInit } from '@angular/core';
 })
 export class ProjectComponent implements AfterViewInit {
 
-  @Input() project = {
+  @Input()project = {
     id: 4,
     date: "11/2024",
     image: "./../../../assets/img/demon_woods_mobile.png",
+    orientation: "landscape",
     title: "Demon Woods",
     skills: "HTML | CSS | JavaScript | Git | Objektorientierung",
     description: "Jump and Run Game. Simple game based on an object-oriented approach. Help the fairy to collect hearts and stones to fight against wraiths and a demon.",
@@ -21,48 +22,25 @@ export class ProjectComponent implements AfterViewInit {
   };
 
   options = {
-    threshold: 1
+    threshold: 0.8
   };
 
-  observer1: IntersectionObserver | undefined;
-  observer2: IntersectionObserver | undefined;
-  observer3: IntersectionObserver | undefined;
-  observer4: IntersectionObserver | undefined;
+  observer: IntersectionObserver | undefined;
 
   constructor() { }
 
-  ngAfterViewInit(): void {
-    this.observer1 = new IntersectionObserver(this.callback1, this.options);
-    this.observer2 = new IntersectionObserver(this.callback2, this.options);
-    this.observer3 = new IntersectionObserver(this.callback3, this.options);
-    this.observer4 = new IntersectionObserver(this.callback4, this.options);
-
-    const target1 = document.querySelector('#project-1');
-    const target2 = document.querySelector('#project-2');
-    const target3 = document.querySelector('#project-3');
-    const target4 = document.querySelector('#project-4');
-
-    if (target1) {
-      this.observer1.observe(target1);
+  ngAfterViewInit(): void {    
+    this.observer = new IntersectionObserver(this.callback, this.options);
+    const project = document.querySelector('#project-' + this.project.id);
+    if (project) {
+      this.observer.observe(project);
     }
-
-    if (target2) {
-      this.observer2.observe(target2);
-    }
-
-    if (target3) {
-      this.observer3.observe(target3);
-    } 
-
-    if (target4) {
-      this.observer4.observe(target4);
-    } 
   }
 
-  callback1 = (entries: IntersectionObserverEntry[]) => {
+  callback = (entries: IntersectionObserverEntry[]) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        this.addClassName('visible');
+        this.addClassNames('visible', 'colored', 'framed');
       }
       // } else {
       //   this.removeClassName('visible');
@@ -70,55 +48,18 @@ export class ProjectComponent implements AfterViewInit {
     });
   };
 
-  callback2 = (entries: IntersectionObserverEntry[]) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.addClassName('visible');
-      }
-      // } else {
-      //   this.removeClassName('visible');
-      // }
-    });
-  };
-
-  callback3 = (entries: IntersectionObserverEntry[]) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.addClassName('visible');
-      }
-      // } else {
-      //   this.removeClassName('visible');
-      // }
-    });
-  };
-
-  callback4 = (entries: IntersectionObserverEntry[]) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.addClassName('visible');
-      }
-      // } else {
-      //   this.removeClassName('visible');
-      // }
-    });
-  };
-
-  addClassName(className: string): void {
-    const target1 = document.querySelector('#project-1');
-    const target2 = document.querySelector('#project-2');
-    const target3 = document.querySelector('#project-3');
-    const target4 = document.querySelector('#project-4');
-    if (target1) {
-      target1.classList.add(className);
+  addClassNames(className1: string, className2: string, className3: string): void {
+    const project = document.querySelector('#project-' + this.project.id);
+    const image = document.querySelector('#project-' + this.project.id + '-image');
+    const border = document.querySelector('#project-' + this.project.id + '-border');
+    if (project) {
+      project.classList.add(className1);      
     }
-    if (target2) {
-      target2.classList.add(className);
+    if (image) {
+      image.classList.add(className2);
     }
-    if (target3) {
-      target3.classList.add(className);
-    }
-    if (target4) {
-      target4.classList.add(className);
+    if (border) {
+      border.classList.add(className3);
     }
   }
 
